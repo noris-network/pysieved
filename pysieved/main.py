a50b10d8337a4ca68b05f6926b3e1091e1fc1332
fioannidis-noris <filippos.ioannidis@noris.gr>
2025-12-04 12:02:12 +0200

    Updated CHANGES and bumped version

--- pysieved/main.py
+++ pysieved/main.py
@@ -18,7 +18,7 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 ## USA
 #
-# 30 May 2025 - Modified by F. Ioannidis.
+# 04 December 2025 - Modified by F. Ioannidis.
 
 
 import optparse
